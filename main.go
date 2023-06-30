1b92588f456b9a60232ddb627be174663a6eab67
areming <AreMingA@163.com>
2023-06-30 12:11:52 +0800

    feibonaqie

--- main.go
+++ main.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
-func main(){
-  fmt.Printf("hello world")
+func main() {
+	fmt.Printf("hello world")
+
+	n := 7
+	k := Fibo3(n)
+	fmt.Printf("%v", k)
+}
+
+func Fibo3(n int) int {
+	if n < 0 {
+		return -1
+	} else {
+		f := Fibonacci()
+		result := 0
+		for i := 0; i < n; i++ {
+			result = f()
+		}
+		return result
+	}
+}
+
+func Fibonacci() func() int {
+	a, b := 0, 1
+	return func() int {
+		a, b = b, a+b
+		return a
+	}
 }
